49f8161f8779022bd2064d100a97c3500711c4ff
Robin Clarke <robin@robinclarke.net>
2011-06-04 21:19:55 +0200

    Used Module Starter to create a build setup

---
+++ lib/SDD.pm
@@ -0,0 +1,110 @@
+package SDD;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+SDD - The great new SDD!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use SDD;
+
+    my $foo = SDD->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robin Clarke, C<< <perl at robinclarke.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-sdd at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SDD>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SDD
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SDD>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/SDD>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/SDD>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/SDD/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Robin Clarke.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of SDD
