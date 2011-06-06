a314943c5c9204607236ad8d0c5531595f788685
Anon <anon@robinclarke.net>
2011-06-06 22:13:35 +0200

    Introduced param validate

--- lib/SDD.pm
+++ lib/SDD.pm
@@ -2,10 +2,15 @@ package SDD;
 
 use warnings;
 use strict;
+use threads;
+use YAML::Any qw/Dump LoadFile/;
+use Log::Log4perl;
+use Params::Validate qw/:all/;
+use File::Basename;
 
 =head1 NAME
 
-SDD - The great new SDD!
+SDD - Shutdown Daemon
 
 =head1 VERSION
 
@@ -18,46 +23,146 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+This is the core of the shutdown daemon script.
 
     use SDD;
+    my $sdd = SDD->new( %args );
+    $sdd->start();
 
-    my $foo = SDD->new();
-    ...
+=head1 METHODS
 
-=head1 EXPORT
+=head2 new
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Create new instance of SDD
 
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
+# TODO: RCL 2011-06-06 Document parameters
 
 =cut
 
 sub new {
     my $class = shift;
+   
     my %params = @_;
-
-    my $self = {};
+    # Remove any undefined parameters from the params hash
+    map{ delete( $params{$_} ) if not $params{$_} } keys %params;
     
-    # TODO: RCL 2011-06-04 Better arg checking
-    foreach( keys( %params ) ){
-        $self->{$_} = $params{$_};
+    # Validate the config file
+    %params = validate_with(
+        params => \%params,
+        spec   => {
+            config   => {
+                callbacks => { 'File exists' => sub{ -f shift } },
+                default   => '/etc/sdd.conf',
+            },
+        },
+        allow_extra => 1,
+    );
+    my $self = {};
+        
+
+    # Read the config file
+    if( not $params{config} ){
+        $params{config} = '/etc/sdd.conf';
     }
+    if( not -f $params{config} ){
+        die( "Config file $params{config} not found\n" );
+    }
+    my $file_config = LoadFile( $params{config} );
+    delete( $params{config} );
+
+    # Merge the default, config file, and passed parameters
+    %params = ( %$file_config, %params );
+
+    my @validate = map{ $_, $params{$_} } keys( %params );
+    %params = validate_with(
+        params => \%params,
+        spec   => 
+            {
+                log_file => {
+                    default   => '/var/log/sdd.log',
+                    callbacks => {
+                        'Log file is writable' => sub{ 
+                            my $filepath = shift;
+                            if( -f $filepath ) {
+                                return -w $filepath;
+                            }
+                            else
+                            {
+                                # Is directory writable
+                                return -w dirname( $filepath );
+                            }
+                        },
+                    },
+                },
+                log_level => {
+                    default   => 'INFO',
+                    regex     => qr/^(DEBUG|INFO|WARN|ERROR)$/,
+                },
+                verbose => {
+                    default => 0,
+                    regex   => qr/^[1|0]$/,
+                },
+                test => {
+                    default => 0,
+                    regex   => qr/^[1|0]$/,
+                },
+                 startup_buffer => {
+                    default => 3600,
+                    regex   => qr/^\d*$/,
+                },
+                monitor => {
+                    type  => ARRAYREF,
+                },
+            },
+        # A little less verbose than Carp...
+        on_fail => sub{ die( shift() ) },
+    );
+
+    $self->{params} = \%params;
+
     bless $self, $class;
+
+    # Set up the logging
+    my  $log4perl_conf = sprintf 'log4perl.rootLogger = %s, Logfile', $params{log_level} || 'WARN';
+    if( $params{verbose} > 0 ){
+        $log4perl_conf .= q(, Screen
+            log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+            log4perl.appender.Screen.stderr  = 0
+            log4perl.appender.Screen.layout   = Log::Log4perl::Layout::PatternLayout
+            log4perl.appender.Screen.layout.ConversionPattern = [%d] %p %m%n
+        );
+
+    }
+
+    $log4perl_conf .= q(
+        log4perl.appender.Logfile          = Log::Log4perl::Appender::File
+        log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
+        log4perl.appender.Logfile.layout.ConversionPattern = [%d] %p %m%n
+    );
+    $log4perl_conf .= sprintf "log4perl.appender.Logfile.filename = %s\n", $params{log_file};
+
+    # ... passed as a reference to init()
+    Log::Log4perl::init( \$log4perl_conf );
+    my $logger = Log::Log4perl->get_logger();
+    $self->{logger} = $logger;
 
     return $self;
 }
 
+=head2 start
+
+Start the shutdown daemon
+
+=cut
 sub start {
     my $self = shift;
     my $logger = $self->{logger};
 
-    sleep( $self->{startup_buffer} );
+    $logger->info( "Started" );
+
+    $logger->info( "Sleeping $self->{params}->{startup_buffer} seconds before starting monitoring" );
+
+    sleep( $self->{params}->{startup_buffer} );
 
     # TODO: RCL 2011-06-04 Load modules here, start threads, etc...
     # This is just a proof of concept for hdparm
@@ -65,7 +170,7 @@ sub start {
         my $shutdown = 1;
 
         # This should be put into an external module, just testing here.
-        foreach my $disk( @{ $self->{disks} } ){
+        foreach my $disk( @{ $self->{monitor}->{hdparam}->{disks} } ){
             $logger->debug( "Testing $disk" );
             my $rtn = `hdparm -C $disk`;
             if( $rtn =~ m/drive state is:  active/s ){
@@ -83,8 +188,8 @@ sub start {
                 exit;
             }
         }
-        $logger->debug( "Sleeping $self->{loop_sleep}" );
-        sleep( $self->{loop_sleep} );
+        $logger->debug( "Sleeping $self->{monitor}->{hdparam}->{loop_sleep}" );
+        sleep( $self->{monitor}->{hdparam}->{loop_sleep} );
     }
 }
 
@@ -100,8 +205,6 @@ the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SDD>.  I w
 automatically be notified of progress on your bug as I make changes.
 
 
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
@@ -113,17 +216,9 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item * Github
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SDD>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/SDD>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/SDD>
+L<https://github.com/robin13/sdd>
 
 =item * Search CPAN
 
